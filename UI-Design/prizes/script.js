230e43924f8e3e1683f5d521701fd4d0991af23c
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-06-08 15:49:12 +0200

    catch different errors

--- UI-Design/prizes/script.js
+++ UI-Design/prizes/script.js
@@ -4,32 +4,39 @@ function OrderItem(awardName) {
     let user = localStorage.getItem("user").substr(1).split("=")[1];
     let tmp = awardName.split(" ");
     let name = "";
-    for(let i = 0; i < tmp.length; i++){
+    for (let i = 0; i < tmp.length; i++) {
         name += tmp[i];
     }
 
-    //let obj = {};
+    let obj = {};
 
     (new Promise((resolve, reject) => {
         $.ajax({
             url: 'http://localhost:3000/users/' + user + "/" + name,
             type: 'POST',
-            //dataType: 'json',
-            //contentType: 'application/json',
-            //data: JSON.stringify(obj),
             crossDomain: true,
             success: function () {
+                alert("Your award will be delivered to you");
                 resolve();
             },
             error: function (xhr, ajaxOptions, thrownError) {
-                let err = (eval("(" + xhr.responseText + ")"));
-                console.log(xhr.status);
-                alert("Error code: " + err.code + ", message: " + err.message);
+                let err = eval("(" + xhr.responseText + ")");
+                //console.log(xhr);
+                //console.log(xhr.statusCode);
+
+                switch (err.code) {
+                    case 412:
+                        alert("You don't have enough points for this award");
+                        break;
+                    default:
+                        alert("Error code: " + err.code + ", message: " + err.message);
+                        break;
+                }
                 reject();
             }
         });
     })).then(() => {
-        alert("Your award will be delivered to you");
+        //alert('uspesno');
     }).catch((error) => {
         //alert('neuspesno');
         console.log(error);
