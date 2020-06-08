76bdb26958052c57e1a75bf4cb0010d4942eff15
Luka Matovic <ml170722d@student.etf.bg.ac.rs>
2020-06-08 15:02:08 +0200

    name of award to camel case

--- UI-Design/prizes/script.js
+++ UI-Design/prizes/script.js
@@ -2,12 +2,17 @@
 function OrderItem(awardName) {
 
     let user = localStorage.getItem("user").substr(1).split("=")[1];
+    let tmp = awardName.split(" ");
+    let name = "";
+    for(let i = 0; i < tmp.length; i++){
+        name += tmp[i];
+    }
 
     //let obj = {};
 
     (new Promise((resolve, reject) => {
         $.ajax({
-            url: 'http://localhost:3000/users/' + user + "/" + awardName,
+            url: 'http://localhost:3000/users/' + user + "/" + name,
             type: 'POST',
             //dataType: 'json',
             //contentType: 'application/json',
